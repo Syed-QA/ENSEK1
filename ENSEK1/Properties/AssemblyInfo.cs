fc4689521e028267eff5432b0c921624198e4f52
SyedQ <SyedQ@DESKTOP-CQ4KR1P>
2021-08-16 20:43:36 +0100

    Add project files.

---
+++ ENSEK1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ENSEK1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ENSEK1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d75f4890-e896-44ad-9e29-6f6bcfac7e7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ENSEK1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ENSEK1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestInitialize]
+
+
+        [TestMethod]
+        public void BuyEnergy()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://ensekautomationcandidatetest.azurewebsites.net/");
+            driver.Manage().Window.Maximize();
+
+
+            IWebElement BuyEnergyBtn = driver.FindElement(By.LinkText(("Buy energy »")));
+            BuyEnergyBtn.Click();
+
+            IWebElement Title = driver.FindElement(By.XPath("//h2[contains(text(),'Buy Energy')]"));
+            Assert.IsTrue(driver.FindElement(By.XPath("//h2[contains(text(),'Buy Energy')]")).Displayed);
+
+            IWebElement uName = driver.FindElement(By.Id("energyType_AmountPurchased"));
+            uName.SendKeys("100");
+
+            IWebElement BuyBtn = driver.FindElement(By.Name("Buy"));
+            BuyBtn.Click();
+            Assert.IsTrue(driver.FindElement(By.XPath("//h2[contains(text(),'Sale Confirmed!')]")).Displayed);
+        }
+     }
+}
